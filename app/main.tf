75a1d79805dbe72280423694a6ca9890f0336f70
john <john.soriao@beehive.hk>
2021-06-20 22:46:15 +0800

    add terraform

---
+++ app/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "=1.18.0"
+    }
+  }
+}
+
+provider "linode" {
+}
+
+resource "linode_lke_cluster" "myapp_cluster" {
+  label       = "myapp"
+  k8s_version = "1.21"
+  region      = "ap-south"
+
+  pool {
+    type  = "g6-standard-1"
+    count = 1
+  }
+}
+
+output "cluster_id" {
+  description = "Cluster Identifier"
+  value       = linode_lke_cluster.myapp_cluster.id
+}
+
+output "api_endpoints" {
+  description = "The endpoints for the Kubernetes API server"
+  value       = linode_lke_cluster.myapp_cluster.api_endpoints
+}
